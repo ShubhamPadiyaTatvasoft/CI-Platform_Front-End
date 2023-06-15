2ab0424ebc265ca3dbae6eb6fcaf23b84793a097
Meet Thakkar <meet.thakkartraining@gmail.com>
2023-06-15 17:35:29 +0530

    User Profile Folder Structure

---
+++ src/app/modules/user-details/components/password-dialog/password-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-dialog',
+  templateUrl: './password-dialog.component.html',
+  styleUrls: ['./password-dialog.component.scss']
+})
+export class PasswordDialogComponent {
+
+}
