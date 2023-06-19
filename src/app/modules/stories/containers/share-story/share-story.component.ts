37a61be711472fc29a27fda83e7576979c22dba9
fenil-kaneriya25 <fenil.kaneriyatraining@gmail.com>
2023-06-19 12:12:36 +0530

    CI-stroies

---
+++ src/app/modules/stories/containers/share-story/share-story.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-share-story',
+  templateUrl: './share-story.component.html',
+  styleUrls: ['./share-story.component.scss']
+})
+export class ShareStoryComponent {
+
+}
