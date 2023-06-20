67078a9aa41d47a52b05bc4f80777a0b641b2b9b
NevilBhut <nevil.bhuttraining@gmail.com>
2023-06-20 15:09:33 +0530

    changes completed

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { FooterComponent } from './components/footer/footer.component';
 import { ContactUsDialogComponent } from './components/contact-us-dialog/contact-us-dialog.component';
 import { HeaderComponent } from './components/header/header.component';
-import { SubHeaderComponent } from './components/sub-header/sub-header.component';
+import { FilterComponent } from './components/filter/filter.component';
 @NgModule({
-
-    exports: [
-        FooterComponent, ContactUsDialogComponent
-    ],
-    declarations: [FooterComponent, ContactUsDialogComponent, HeaderComponent, SubHeaderComponent]
+  exports: [
+    FooterComponent,
+    ContactUsDialogComponent,
+    HeaderComponent,
+    FilterComponent,
+  ],
+  declarations: [
+    FooterComponent,
+    ContactUsDialogComponent,
+    HeaderComponent,
+    FilterComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
