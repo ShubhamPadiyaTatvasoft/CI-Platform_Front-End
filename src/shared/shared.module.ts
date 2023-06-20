e71f687d16be322c2a4fc4ebad2b322748dc658d
NevilBhut <nevil.bhuttraining@gmail.com>
2023-06-20 11:42:12 +0530

    folder-structure for header, subheader

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FooterComponent } from './components/footer/footer.component';
 import { ContactUsDialogComponent } from './components/contact-us-dialog/contact-us-dialog.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SubHeaderComponent } from './components/sub-header/sub-header.component';
 @NgModule({
 
     exports: [
         FooterComponent, ContactUsDialogComponent
     ],
-    declarations: [FooterComponent, ContactUsDialogComponent]
+    declarations: [FooterComponent, ContactUsDialogComponent, HeaderComponent, SubHeaderComponent]
 })
 export class SharedModule { }
