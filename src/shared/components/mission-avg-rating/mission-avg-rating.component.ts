1943d86be7e980f88bfd0430900e3e5b775e1db9
AmanGandhi2305 <mahammadaman.gandhitraining@gmail.com>
2023-06-20 16:54:46 +0530

    Cards Folder Structure

---
+++ src/shared/components/mission-avg-rating/mission-avg-rating.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-avg-rating',
+  templateUrl: './mission-avg-rating.component.html',
+  styleUrls: ['./mission-avg-rating.component.scss']
+})
+export class MissionAvgRatingComponent {
+
+}
---
+++ src/shared/components/mission-story-cards/mission-story-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-story-cards',
+  templateUrl: './mission-story-cards.component.html',
+  styleUrls: ['./mission-story-cards.component.scss']
+})
+export class MissionStoryCardsComponent {
+
+}
