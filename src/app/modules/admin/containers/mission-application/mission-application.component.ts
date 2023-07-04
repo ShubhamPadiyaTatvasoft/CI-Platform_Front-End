d66c2e67bccdbd39cd072c9f91361da5297bbd10
harshtatva2002 <harsh.pateltraining@gmail.com>
2023-07-04 11:47:33 +0530

    mission application and story crud done

--- src/app/modules/admin/containers/mission-application/mission-application.component.ts
+++ src/app/modules/admin/containers/mission-application/mission-application.component.ts
@@ -1,10 +1,138 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { AdminService } from '../../services/admin.service';
+import { LoginServiceService } from 'src/app/modules/auth/services/login-service.service';
+import { Router } from '@angular/router';
+import { ConfirmBoxService } from '../../services/confirm-box.service';
+import { NotificationService } from 'src/shared/services/notification.service';
+import { ErrorMessages } from 'src/app/common/errorMsg.static';
 
 @Component({
   selector: 'app-mission-application',
   templateUrl: './mission-application.component.html',
-  styleUrls: ['./mission-application.component.scss']
+  styleUrls: ['./mission-application.component.scss'],
 })
-export class MissionApplicationComponent {
+export class MissionApplicationComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  data: any;
+  timer: any;
+  dataSource = new MatTableDataSource<any>();
+  displayedColumn: string[] = [
+    'MissionTitle',
+    'MissionId',
+    'UserId',
+    'UserName',
+    'MissionApplicationId',
+    'action',
+  ];
+  missionApplications: any[] = [];
+  mission: any[] = [];
+  showNoDataFound: string;
+  alertMsg: string;
 
+  constructor(
+    private adminService: AdminService,
+    private loginService: LoginServiceService,
+    private router: Router,
+    private dialogService: ConfirmBoxService,
+    private notifyService: NotificationService
+  ) {}
+
+  ngOnInit(): void {
+    this.getMissionApplicationData('');
+  }
+
+  //event called when user start searching
+  keyupSearch(search: any) {
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => {
+      this.getMissionApplicationData(search.value);
+    }, 1000);
+  }
+
+  //get all mission Application data from api
+  getMissionApplicationData(search: string) {
+    this.adminService.getAllMissionApplication(search).subscribe({
+      next: (res) => {
+        this.missionApplications = [];
+        for (let i = 0; i < res.data['missions'].length; i++) {
+          for (let j = 0; j < res.data['applications'].length; j++) {
+            for (let k = 0; k < res.data['users'].length; k++) {
+              if (
+                res.data['users'][k].userId ==
+                res.data['applications'][j].userId
+              ) {
+                if (
+                  res.data['applications'][j].missionId ==
+                  res.data['missions'][i].missionId
+                ) {
+                  let missionApplication = {
+                    missionId: res.data['missions'][i].missionId,
+                    missionTitle: res.data['missions'][i].title,
+                    missionApplicationId:
+                      res.data['applications'][j].missionApplicationId,
+                    userName:
+                      res.data['users'][k].firstName +
+                      ' ' +
+                      res.data['users'][k].lastName,
+                    userId: res.data['users'][k].userId,
+                    appliedDate: res.data['applications'][j].appliedAt,
+                  };
+                  this.missionApplications.push(missionApplication);
+                }
+              }
+            }
+          }
+        }
+        this.dataSource = new MatTableDataSource(this.missionApplications);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        if (this.missionApplications.length == 0) {
+          this.showNoDataFound = 'No data found';
+        } else {
+          this.showNoDataFound = '';
+        }
+      },
+      error: (err) => {
+        this.notifyService.showError(ErrorMessages.ApiErrorMessage.ApiFailed);
+        this.showNoDataFound = 'No data found';
+      },
+    });
+  }
+
+  //approve reject missionApplication api call
+  missionApplicationApproveReject(event: any, action: string) {
+    if (action == 'rejected') {
+      this.alertMsg = 'reject';
+    } else {
+      this.alertMsg = 'approve';
+    }
+    this.dialogService
+      .openConfirmDialogue('Are you sure want to ' + this.alertMsg + '!!?')
+      .afterClosed()
+      .subscribe((res) => {
+        if (res == true) {
+          this.adminService
+            .approveRejectMissionApplication(+event.id, action)
+            .subscribe({
+              next: (res) => {
+                if (res.statusCode == 200) {
+                  this.notifyService.showSuccess(res.message);
+                  this.getMissionApplicationData('');
+                } else {
+                  this.notifyService.showSuccess(res.message);
+                }
+              },
+              error: (err) => {
+                this.notifyService.showError(
+                  ErrorMessages.ApiErrorMessage.ApiFailed
+                );
+              },
+            });
+        }
+      });
+  }
 }
--- src/app/modules/admin/containers/story/story.component.ts
+++ src/app/modules/admin/containers/story/story.component.ts
@@ -1,10 +1,133 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { AdminService } from '../../services/admin.service';
+import { LoginServiceService } from 'src/app/modules/auth/services/login-service.service';
+import { Router } from '@angular/router';
+import { ConfirmBoxService } from '../../services/confirm-box.service';
+import { NotificationService } from 'src/shared/services/notification.service';
+import { ErrorMessages } from 'src/app/common/errorMsg.static';
 
 @Component({
   selector: 'app-story',
   templateUrl: './story.component.html',
-  styleUrls: ['./story.component.scss']
+  styleUrls: ['./story.component.scss'],
 })
-export class StoryComponent {
+export class StoryComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  data: any;
+  timer: any;
+  dataSource = new MatTableDataSource<any>();
+  displayedColumn: string[] = [
+    'StoryTitle',
+    'FullName',
+    'MissionTitle',
+    'Action',
+  ];
+  stories: any[] = [];
+  showNoDataFound: string;
+  alertMsg: string;
 
+  constructor(
+    private adminService: AdminService,
+    private loginService: LoginServiceService,
+    private router: Router,
+    private dialogService: ConfirmBoxService,
+    private notifyService: NotificationService
+  ) {}
+
+  ngOnInit(): void {
+    this.getStoryData('');
+  }
+
+  //event called when user start searching
+  keyupSearch(search: any) {
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => {
+      this.getStoryData(search.value);
+    }, 1000);
+  }
+
+  //get all story data from api
+  getStoryData(search: string) {
+    this.adminService.getAllStories(search).subscribe({
+      next: (res) => {
+        this.stories = [];
+        for (let i = 0; i < res.data['missions'].length; i++) {
+          for (let j = 0; j < res.data['stories'].length; j++) {
+            for (let k = 0; k < res.data['users'].length; k++) {
+              if (
+                res.data['users'][k].userId == res.data['stories'][j].userId
+              ) {
+                if (
+                  res.data['stories'][j].missionId ==
+                  res.data['missions'][i].missionId
+                ) {
+                  let story = {
+                    StoryTitle: res.data['stories'][j].title,
+                    missionTitle: res.data['missions'][i].title,
+                    storyId: res.data['stories'][j].storyId,
+                    fullName:
+                      res.data['users'][k].firstName +
+                      ' ' +
+                      res.data['users'][k].lastName,
+                  };
+                  this.stories.push(story);
+                }
+              }
+            }
+          }
+        }
+        this.dataSource = new MatTableDataSource(this.stories);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        if (this.stories.length == 0) {
+          this.showNoDataFound = 'No data found';
+        } else {
+          this.showNoDataFound = '';
+        }
+      },
+      error: (err) => {
+        this.notifyService.showError(ErrorMessages.ApiErrorMessage.ApiFailed);
+        this.showNoDataFound = 'No data found';
+      },
+    });
+  }
+
+  //approve reject delete story api call
+  storyApproveRejectDelete(event: any, action: string) {
+    if (action == 'rejected') {
+      this.alertMsg = 'reject';
+    } else if (action == 'approved') {
+      this.alertMsg = 'approve';
+    } else {
+      this.alertMsg = action;
+    }
+    this.dialogService
+      .openConfirmDialogue('Are you sure want to ' + this.alertMsg + '!!?')
+      .afterClosed()
+      .subscribe((res) => {
+        if (res == true) {
+          this.adminService
+            .approveRejectDeleteStory(event.id, action)
+            .subscribe({
+              next: (res) => {
+                if (res.statusCode == 200) {
+                  this.notifyService.showSuccess(res.message);
+                  this.getStoryData('');
+                } else {
+                  this.notifyService.showSuccess(res.message);
+                }
+              },
+              error: (err) => {
+                this.notifyService.showError(
+                  ErrorMessages.ApiErrorMessage.ApiFailed
+                );
+              },
+            });
+        }
+      });
+  }
 }
